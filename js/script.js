2b1bb79c343be0858795d1306e78d76ea61b2a5b
jerry_w <jweber@gmx.com>
2023-07-27 20:11:20 -0500

    Completed JavaScript

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const createPet = function (name, species) {
   const pet = {
     name: name,
     species: species,
-    isTired: 5, // Scale from 1 (refreshed) to 10 (exhausted)
+    isTired: 5,
     sleep: function () {
       console.log(`${this.name} needs nap. Zzz...`);
       this.isTired = 1;
@@ -30,9 +30,25 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-console.log(sora, clover, baxter, cleo, francine);
+clover.isTired = 8;
+francine.isTired = 9;
 
-clover.sleep();
-baxter.play();
+const allPets = [sora, clover, baxter, cleo, francine];
 
-console.log(clover, baxter);
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
